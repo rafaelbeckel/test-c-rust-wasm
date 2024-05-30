fbfb577040527fb3a24ae5ad7da49ca56fc6c055
Surma <surma@surma.dev>
2024-05-30 11:38:49 +0100

    Skip LLVM IR and remove copy steps

--- build_both.sh
+++ build_both.sh
@@ -2,16 +2,6 @@
 ./build_c.sh
 
 # Link c_maths.ll and r_maths.ll to a single WebAssembly binary
-llc \
-    -march=wasm32 \
-    -filetype=obj \
-    c_maths.ll
-
-llc \
-    -march=wasm32 \
-    -filetype=obj \
-    r_maths.ll
-
 wasm-ld \
     --no-entry \
     --export-all \
--- build_rust.sh
+++ build_rust.sh
@@ -1,6 +1,13 @@
-RUSTFLAGS="--cfg=web_sys_unstable_apis --Z wasm_c_abi=spec" cargo rustc --target=wasm32-unknown-unknown --release -- --emit=llvm-ir
+export RUSTFLAGS="--cfg=web_sys_unstable_apis --Z wasm_c_abi=spec" 
+rustc +nightly \
+  --target=wasm32-unknown-unknown \
+  -C opt-level=3 \
+  --crate-type=cdylib \
+  --emit=obj \
+  -o r_maths.o \
+  src/lib.rs
 
-cp target/wasm32-unknown-unknown/release/deps/maths.ll r_maths.ll
+# cp target/wasm32-unknown-unknown/release/maths.wasm r_maths.wasm
 
 # built in build_both.sh
 # cp target/wasm32-unknown-unknown/release/maths.wasm r_maths.wasm
