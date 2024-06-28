c8a7b7f9040ebecb4985b115662a0ab9ece08b6e
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 22:59:08 +0200

    updates all READMEs

---
+++ crates/6_rust_bindgen/build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut build = cc::Build::new();
+
+    if let Some(libc) = std::env::var_os("DEP_WASM32_UNKNOWN_UNKNOWN_OPENBSD_LIBC_INCLUDE") {
+        build.include(libc);
+        println!("cargo::rustc-link-lib=wasm32-unknown-unknown-openbsd-libc");
+    }
+
+    build.include("src");
+    build.file("src/calculator.c");
+    build.file("src/memory.c");
+    build.compile("c_maths");
+}
