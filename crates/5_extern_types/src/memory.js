f810a5894dad4137e8e8250455028bf3291e9f8a
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 22:58:55 +0200

    updates all READMEs

--- crates/5_extern_types/src/memory.js
+++ crates/5_extern_types/src/memory.js
@@ -1,40 +1,33 @@
-function __wbg_init_memory(imports, maybe_memory) {
-  if (!imports.env) {
-    imports.env = {};
+let env = {};
+
+const u16_max = 65536;
+
+env.memory = new WebAssembly.Memory({ initial: 1 });
+
+env.malloc = (size) => {
+  const ptr = env.memory.buffer.byteLength;
+  env.memory.grow(Math.ceil(size / u16_max));
+  return ptr;
+};
+
+env.realloc = function (ptr, size) {
+  const mem = env.memory;
+  const old = mem.buffer.byteLength;
+  const old_pages = Math.ceil(old / u16_max);
+  const new_pages = Math.max(old_pages, Math.ceil((ptr + size) / u16_max));
+  if (new_pages > old_pages) {
+    mem.grow(new_pages - old_pages);
   }
+  return ptr;
+};
 
-  const u16_max = 65536;
+env.free = function (ptr, size) {
+  /* no-op */
+};
 
-  imports.env.memory = maybe_memory || new WebAssembly.Memory({ initial: 1 });
+const malloc = env.malloc;
+const realloc = env.realloc;
+const free = env.free;
 
-  imports.env.malloc = (size) => {
-    if (typeof wasm.__wbindgen_malloc === "function") {
-      return wasm.__wbindgen_malloc(size, 1);
-    } else {
-      const ptr = imports.env.memory.buffer.byteLength;
-      imports.env.memory.grow(Math.ceil(size / u16_max));
-      return ptr;
-    }
-  };
-
-  imports.env.realloc = function (ptr, size) {
-    if (typeof wasm.__wbindgen_realloc === "function") {
-      return wasm.__wbindgen_realloc(ptr, size, 1);
-    } else {
-      const mem = imports.env.memory;
-      const old = mem.buffer.byteLength;
-      const old_pages = Math.ceil(old / u16_max);
-      const new_pages = Math.max(old_pages, Math.ceil((ptr + size) / u16_max));
-      if (new_pages > old_pages) {
-        mem.grow(new_pages - old_pages);
-      }
-      return ptr;
-    }
-  };
-
-  imports.env.free = function (ptr, size) {
-    if (typeof wasm.__wbindgen_free === "function") {
-      wasm.__wbindgen_free(ptr, size, 1);
-    }
-  };
-}
+export { malloc, realloc, free };
+export default env;
