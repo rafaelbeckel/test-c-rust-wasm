9e0156c98289cab1315a0ef633fd2d760dbbe227
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 22:14:13 +0200

    removes old files

--- build_rust.sh
+++
@@ -1,4 +0,0 @@
-export RUSTFLAGS="--cfg=web_sys_unstable_apis --Z wasm_c_abi=spec"
-cargo +nightly build --target=wasm32-unknown-unknown --release
-
-cp target/wasm32-unknown-unknown/release/libmaths.rlib r_maths.a
