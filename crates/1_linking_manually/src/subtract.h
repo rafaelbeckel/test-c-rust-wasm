72aaa1145270349727def3329ef56f58f8e7c61f
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 05:23:20 +0200

    updates 1_linking_manually int types

--- crates/1_linking_manually/src/subtract.h
+++ crates/1_linking_manually/src/subtract.h
@@ -1,11 +1,6 @@
-#ifndef F3514DCC_BF20_4EE0_B65D_4287FAC302B9
-#define F3514DCC_BF20_4EE0_B65D_4287FAC302B9
 #ifndef SUBTRACT_H
 #define SUBTRACT_H
 
 unsigned int subtract(unsigned int a, unsigned int b);
 
 #endif // SUBTRACT_H
-
-
-#endif /* F3514DCC_BF20_4EE0_B65D_4287FAC302B9 */
