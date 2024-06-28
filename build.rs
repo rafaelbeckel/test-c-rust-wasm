5f22983adfc56c75ccff68dac66e4254f119658b
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 22:14:13 +0200

    removes old files

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    cc::Build::new().file("wasm/maths.c").compile("c_maths");
-}
--- wasm/subtract.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUBTRACT_H
-#define SUBTRACT_H
-
-unsigned int subtract(unsigned int a, unsigned int b);
-
-#endif // SUBTRACT_H
