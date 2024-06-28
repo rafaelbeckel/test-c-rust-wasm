52f9e2926c9ac54de922ab4f8cf6b5ed5caf57ec
Rafael Beckel <rafaelbeckel@gmail.com>
2024-06-28 22:02:53 +0200

    adds project 4: wasm-bindgen

--- crates/4_wasm_bindgen/README.md
+++ crates/4_wasm_bindgen/README.md
@@ -3,3 +3,5 @@
 ## Project 4: Wasm-Bindgen
 
 TBD
+
+What if the Calculator is defined in C and you cannot change it
---
+++ crates/4_wasm_bindgen/build_with_wasm_pack.sh
@@ -0,0 +1,18 @@
+# As of June 2024, it is necessary to use nightly with the wasm_c_abi=spec
+# flag that was introduced in April.
+
+# The `spec` ABI will be the default compiler behavior in the future.
+# If the future has arrived and the flag is not necessary anymore,
+# please open an Issue or submit PR to update this script.
+
+export RUSTFLAGS="--Z wasm_c_abi=spec"
+wasm-pack build --target web --release
+
+# Replaces the import statement from 'env' to './env.js'
+sed -i '' "s/from 'env';/from '.\/env.js';/g" pkg/wbg_calculator.js
+
+# Copy the memory.js file to the pkg folder
+cp src/memory.js pkg/env.js
+
+# Optional: Feel free to read the generated WAT file
+wasm2wat pkg/wbg_calculator_bg.wasm >pkg/wbg_calculator.wat
